385d3f32a1411646973e10cdc75293411283889e
Pawel Zak <pawel.zak@live.com>
2017-08-14 20:20:51 +0200

    disable superfluous endpoints

--- src/main/java/pawelzak/moviedb/MoviedbApplication.java
+++ src/main/java/pawelzak/moviedb/MoviedbApplication.java
@@ -2,8 +2,9 @@ package pawelzak.moviedb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class MoviedbApplication {
 
   public static void main(String[] args) {
